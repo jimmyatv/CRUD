60ef3ea2ac5eb76c44182e637722b75d1b8fcb0a
jimmyatv <rasic.alexandar@gmail.com>
2023-02-02 00:50:33 +0100

    CRUD

---
+++ app.js
@@ -0,0 +1,7 @@
+// * CRUD - Create, Read, Update and Delete
+
+const inputName = document.querySelector('.inputName');
+const inputLastName = document.querySelector('.inputLastName');
+const inputAge = document.querySelector('.inputAge');
+const btnAdd = document.querySelector('.btnAdd');
+const btnSave = document.querySelector('.btnSave');
